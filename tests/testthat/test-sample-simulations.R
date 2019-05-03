f15ecbf372b17153896915bc204edfb43e80d8dd
PnoJai <jai@jeffryes.net>
2019-05-03 13:36:55 -0400

    Eugene's fix ready for master.

--- tests/testthat/test-sample-simulations.R
+++ tests/testthat/test-sample-simulations.R
@@ -1,58 +1,60 @@
 context("test-sample-simulations")
 
-test_that("CV simulation matrix matches sample.", {
-        # Open the Excel example.
-        # Read the data range you're interested in, namely the matrix.
-        # The optimized approach eliminates the right half of the matrix. Just consider the left.
-
-        library(openxlsx)
-        library(readr)
-
-        # Read simulation
-        xlsx <- readWorkbook("./../testdata/RW_CV_corrected.xlsx", sheet = 1, startRow = 8,
-                             skipEmptyRows = FALSE, colNames = FALSE, cols = c(5:31))
-        
-        # Make a vector of the timestamp column
-        time_sec_exists <- !is.na(xlsx[,1])
-        time_sec <- c(0, xlsx[time_sec_exists, 1])
-
-        # Pick off odd rows, ignoring the intermediate Michaelis-Menten correction.
-        result_rows_boolean <- c(rep(c(TRUE, FALSE), floor(nrow(xlsx) / 2)), TRUE)
-        xlsx_results <- xlsx[result_rows_boolean, -1] # Don't use the time stamp column yet
-
-        # Combine the timestamp column and the results matrix.
-        xlsx_results <- cbind(time_sec, xlsx_results)
-        xlsx_results[is.na(xlsx_results)] <- 0
-
-        write_csv(xlsx_results, "./../testdata/RW_CV_simulation_as_read.csv")        
-        
-        # Run the simulation.
-        # Random Walk.
-        # Build CV model and plot it.
-
-        library(tidyverse)
-
-        vmax <- 4.57
-        km <- 0.78
-        release <- 2.75
-
-        pulses <- 1
-        pulse_freq <- 1
-        bin_size <- 2.0
-        electrode_distance <- 50
-        dead_space_distance <- 4
-        diffusion_coefficient <- 2.7 * 10^-6
-        duration = 0.94815
-
-        rw <- rwalk_cv_pulse(vmax, km, release, pulses, pulse_freq, bin_size, electrode_distance,
-                             dead_space_distance, diffusion_coefficient, duration)
-
-        write_csv(rw, "./../testdata/RW_CV_rwalk_computed.csv") 
-        
-        rw_left_side = rw[, 1:27] # Disregard bins to the right of the electrode
-        
-        expect_equivalent(xlsx_results, rw_left_side)
-})
+# Test passed 5/3. Then filtered the zero rows. The electrode test works and is all
+# that is necessary now.
+# test_that("CV simulation matrix matches sample.", {
+#         # Open the Excel example.
+#         # Read the data range you're interested in, namely the matrix.
+#         # The optimized approach eliminates the right half of the matrix. Just consider the left.
+# 
+#         library(openxlsx)
+#         library(readr)
+# 
+#         # Read simulation
+#         xlsx <- readWorkbook("./../testdata/RW_CV_corrected.xlsx", sheet = 1, startRow = 8,
+#                              skipEmptyRows = FALSE, colNames = FALSE, cols = c(5:31))
+#         
+#         # Make a vector of the timestamp column
+#         time_sec_exists <- !is.na(xlsx[,1])
+#         time_sec <- c(0, xlsx[time_sec_exists, 1])
+# 
+#         # Pick off odd rows, ignoring the intermediate Michaelis-Menten correction.
+#         result_rows_boolean <- c(rep(c(TRUE, FALSE), floor(nrow(xlsx) / 2)), TRUE)
+#         xlsx_results <- xlsx[result_rows_boolean, -1] # Don't use the time stamp column yet
+# 
+#         # Combine the timestamp column and the results matrix.
+#         xlsx_results <- cbind(time_sec, xlsx_results)
+#         xlsx_results[is.na(xlsx_results)] <- 0
+# 
+#         write_csv(xlsx_results, "./../testdata/RW_CV_simulation_as_read.csv")        
+#         
+#         # Run the simulation.
+#         # Random Walk.
+#         # Build CV model and plot it.
+# 
+#         library(tidyverse)
+# 
+#         vmax <- 4.57
+#         km <- 0.78
+#         release <- 2.75
+# 
+#         pulses <- 1
+#         pulse_freq <- 1
+#         bin_size <- 2.0
+#         electrode_distance <- 50
+#         dead_space_distance <- 4
+#         diffusion_coefficient <- 2.7 * 10^-6
+#         duration = 0.94815
+# 
+#         rw <- rwalk_cv_pulse(vmax, km, release, pulses, pulse_freq, bin_size, electrode_distance,
+#                              dead_space_distance, diffusion_coefficient, duration)
+# 
+#         write_csv(rw, "./../testdata/RW_CV_rwalk_computed.csv") 
+#         
+#         rw_left_side = rw[, 1:27] # Disregard bins to the right of the electrode
+#         
+#         expect_equivalent(xlsx_results, rw_left_side)
+# })
 
 test_that("CV simulation electrode matches sample", {
         # Open the Excel example.
@@ -104,7 +106,11 @@ test_that("CV simulation electrode matches sample", {
         
         # write_csv(rw, "./../testdata/RW_CV_rwalk_electrode_computed.csv") 
         
-        expect_equivalent(xlsx_results, rw)
+        # Spreadsheet by design only uses every other time iteration at the electrod.
+        included <- seq(1, nrow(xlsx_results), 2)
+        
+        
+        expect_equivalent(xlsx_results[included, ], rw)
 })
 
 test_that("CV simulation releases match corrected sample", {
