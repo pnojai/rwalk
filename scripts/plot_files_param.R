ecc4104b7463998e5e1d3c56f463fbdbfa5f5664
PnoJai <jai@jeffryes.net>
2019-06-02 10:13:04 -0400

    4 WT, 2 KO

--- scripts/plot_files_param.R
+++ scripts/plot_files_param.R
@@ -14,7 +14,7 @@ if (fil_not_exists) {stop("Input file not found")}
 
 # Pick a file to work on.
 print(fils)
-i <- 5
+i <- 6
 
 fil_params_cur <- fil_params_all[fil_params_all$filename == fils[i], ]
 sample_rate <- head(fil_params_cur$sample_rate, 1) # milliseconds
